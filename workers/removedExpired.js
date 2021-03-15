3abbecc8a172f4e879bc5d3558aabd71dcce0a56
chemazu <chukwuemekachemazu@gmail.com>
2021-03-15 14:01:44 +0100

    increased alert  expiry time

--- workers/removedExpired.js
+++ workers/removedExpired.js
@@ -5,7 +5,7 @@ const removeExpired = new CronJob("*/10 * * * * *", async () => {
   const allAlerts = await alerts.find();
   allAlerts.forEach(async (alert) => {
     if (
-      new Date(alert.createdAt).getTime() + 10 * 10000 <
+      new Date(alert.createdAt).getTime() + 25 * 10000 <
       new Date().getTime()
     ) {
       await alerts.deleteOne({ _id: alert._id });
